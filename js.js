54280add7decf69b8a3193ca92788f89c68ac4a1
ClashofClansDeveloperGEMseller <89543834+ClashofClansDeveloperGEMseller@users.noreply.github.com>
2021-08-26 02:22:46 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+function load(){
+  alert("Smart load only is available payment method right now!")
+  window.open("LoadPayment.html")
+  
+}
+function gcash(){
+  alert("Gcash Payment is not available right now!")
+  
+}
